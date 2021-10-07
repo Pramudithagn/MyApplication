41ca20a14ca9b6ccdd2d1d71c1a582c40b3cb4b6
Pramudithagn <76003331+Pramudithagn@users.noreply.github.com>
2021-10-07 23:44:40 +0530

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/markleave.java
@@ -0,0 +1,14 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class markleave extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_markleave);
+    }
+}
